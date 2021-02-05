69940a83644a74d905f1087cbe9be2fcf2052ea6
TomaszBeben <tbeben@st.swps.edu.pl>
2021-02-05 14:43:17 +0100

    button complete

--- src/components/MenuButton.js
+++ src/components/MenuButton.js
@@ -1,8 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const MenuButton = () => {
+    const [rotate, setRotate] = useState('menu-square')
+
+
+    function functionRotate() {
+        return new Promise(
+            res => {
+                setTimeout(() => {
+                    res(setRotate('menu-square'))
+                }, 300)
+            }
+        );
+    }
+
+    async function rotateCall() {
+        setRotate('menu-square rotate')
+        await functionRotate()
+    }
+
     return (
-        <div className='menu-square rotate'>
+        <div className={rotate} onClick={rotateCall}>
             <div className='menu-dots dot-1'></div>
             <div className='menu-dots dot-2'></div>
             <div className='menu-dots dot-3'></div>
