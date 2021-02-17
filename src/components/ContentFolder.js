1a6c34d3b6b1affff1a4e5017a8bc08b21f72e61
TomaszBeben <tbeben@st.swps.edu.pl>
2021-02-17 22:25:33 +0100

    draggable test works

--- src/components/ContentFolder.js
+++ src/components/ContentFolder.js
@@ -1,21 +1,17 @@
 import React from 'react'
-import Draggable, { DraggableCore } from 'react-draggable';
-console.log(window.outerHeight);
-console.log(window.outerWidth);
+import Draggable from 'react-draggable';
 
 const ContentFolder = () => {
     return (
         <>
             <div className='content-folder-area'>
-                <DraggableCore >
-                    <Draggable handle='.top' defaultPosition={{ x: 50, y: 100 }} bounds='.content-folder-area'>
-                        <div className='draggable-library'>
-                            <div className='top'></div>
-                            <div className='bottom'></div>
-                            <button></button>
-                        </div>
-                    </Draggable>
-                </DraggableCore>
+                <Draggable handle='.top' defaultPosition={{ x: 50, y: 100 }} bounds='.content-folder-area'>
+                    <div className='draggable-library'>
+                        <div className='top'></div>
+                        <div className='bottom'></div>
+                        <button></button>
+                    </div>
+                </Draggable>
             </div>
         </>
     )
