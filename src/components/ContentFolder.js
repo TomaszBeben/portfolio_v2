ae1dd8eb1224ce172440177c1104140a9d621c3e
TomaszBeben <tbeben@st.swps.edu.pl>
2021-02-21 22:53:28 +0100

    content folder first view

--- src/components/ContentFolder.js
+++ src/components/ContentFolder.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import Draggable from 'react-draggable';
+import FolderCard from './FolderCard';
 
 const ContentFolder = () => {
     return (
@@ -7,9 +8,14 @@ const ContentFolder = () => {
             <div className='content-folder-area'>
                 <Draggable handle='.content-folder-toolbar' defaultPosition={{ x: 50, y: 100 }} bounds='.main--Page--Background'>
                     <div className='draggable-library'>
-                        <div className='content-folder-toolbar'></div>
+                        <div className='content-folder-toolbar'>
+                            <div className='content-folder-toolbar-nav'>_</div>
+                            <div className='content-folder-toolbar-nav'>[]</div>
+                            <div className='content-folder-toolbar-nav'>x</div>
+                        </div>
                         <div className='content-folder-content'>
-                        <div className='content-folder-resize-handle'></div>
+                            <FolderCard/>
+                        <   div className='content-folder-resize-handle'></div>
                         </div>
                     </div>
                 </Draggable>
---
+++ src/components/FolderCard.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const FolderCard = () => {
+    return (
+        <div className='folder-card'>
+            <div className='folder-card-text'>text</div>
+        </div>
+    )
+}
+
+export default FolderCard
