6ebb5c14dfd93c2dca290026f9ba0a4d21b5f125
TomaszBeben <tbeben@st.swps.edu.pl>
2021-03-03 23:47:34 +0100

    check

--- src/components/ContentFolder.js
+++ src/components/ContentFolder.js
@@ -5,19 +5,24 @@ import FolderCard from './FolderCard';
 
 
 const ContentFolder = (props) => {
+    const random = (num) => Math.floor(Math.random() * num);
+    console.log(window.innerHeight);
 
     return (
         <>
             <div className={props.isVisible}>
-                <Draggable handle='.content-folder-toolbar' defaultPosition={{ x: 10, y: 10 }} bounds='.main--Page--Background'>
+                <Draggable
+                    handle='.content-folder-toolbar'
+                    defaultPosition={
+                        { x: random(window.innerWidth - 400), y: random(window.innerHeight - 400) }}
+                    bounds='.main--Page--Background'>
                     <div className='draggable-library'>
                         <div className='content-folder-toolbar'>
                             <div className='content-folder-toolbar-nav'>[]</div>
                             <div className='content-folder-toolbar-nav' onClick={props.hideContentFolder}>x</div>
                         </div>
                         <div className='content-folder-content'>
-                            <FolderCard/>
-                        <   div className='content-folder-resize-handle'></div>
+                            <FolderCard />
                         </div>
                     </div>
                 </Draggable>
--- src/components/MenuButton.js
+++ src/components/MenuButton.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react'
 
 import ContentFolder from './ContentFolder'
 import NavElements from './NavElements';
-import {toggle} from './ToggleFunction';
+import { toggle } from './ToggleFunction';
 
 const MenuButton = () => {
     const [rotate, setRotate] = useState('menu-square')
@@ -26,7 +26,7 @@ const MenuButton = () => {
 
     const show = 'nav-elements-container'
     const hide = 'nav-elements-container nav-elements-hide'
-    const[elemHide, setElemHide]= useState(hide)
+    const [elemHide, setElemHide] = useState(hide)
 
     const hideElem = 'content-folder-area hide'
     const showElem = 'content-folder-area'
@@ -34,19 +34,26 @@ const MenuButton = () => {
 
     return (
         <>
-        <div className='menu-nav-container'>
-            <NavElements
-                show={elemHide}
-                click={()=>{toggle(elemHide, setElemHide, hide, show); setVisible(showElem)}}
-            />
-            <div className={rotate} onClick={()=>{rotateCall(); toggle(elemHide, setElemHide, hide, show)}}>
-                <div className='menu-dots dot-1'></div>
-                <div className='menu-dots dot-2'></div>
-                <div className='menu-dots dot-3'></div>
-                <div className='menu-dots dot-4'></div>
+            <div className='menu-nav-container'>
+                <NavElements
+                        show={elemHide}
+                        click={() => {
+                            toggle(elemHide, setElemHide, hide, show)
+                            setVisible(showElem)
+                    }}
+                />
+                <div className={rotate} onClick={() => {
+                    rotateCall()
+                    toggle(elemHide, setElemHide, hide, show)
+                }}
+                >
+                    <div className='menu-dots dot-1'></div>
+                    <div className='menu-dots dot-2'></div>
+                    <div className='menu-dots dot-3'></div>
+                    <div className='menu-dots dot-4'></div>
+                </div>
             </div>
-        </div>
-        <ContentFolder hideContentFolder={()=>setVisible(hideElem)} isVisible={isVisible}/>
+            <ContentFolder hideContentFolder={() => setVisible(hideElem)} isVisible={isVisible} />
         </>
     )
 }
