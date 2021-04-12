6688bc364a8a080c49ae8c9a1401f7ee6850987c
TomaszBeben <tbeben@st.swps.edu.pl>
2021-04-12 20:58:48 +0200

    about languages

--- src/components/ContentComponents/AboutComponent.js
+++ src/components/ContentComponents/AboutComponent.js
@@ -1,17 +1,34 @@
-import React from 'react'
+import React,{useState} from 'react'
+import { language as text } from './Language'
+import {toggle} from '../GlobalFunction'
 
 const AboutComponent = () => {
+    const [language, setLanguage] = useState(text.about.pl)
+    const [languageButton, setLanguageButton] = useState("EN")
+    const style={
+        width: '100px',
+        height: '50px',
+        fontSize: '40px',
+        color: 'white',
+        backgroundColor: 'black',
+        borderRadius: '10px'
+    }
     return (
-        <div className= 'about-component'>
-            <header className='about-component-header'>TBEBEN</header>
-            <h1 className='about-component-text'>
-                Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-                Enim, molestiae ratione quos laboriosam explicabo aut magnam.
-                Delectus ea explicabo, architecto veniam unde beatae dicta praesentium ad dignissimos,
-                quos qui provident?
-            </h1>
-            <div className="about-component-imgOfMyself"/>
-        </div>
+        <>
+            <button
+            style={style}
+            onClick={()=>{
+                toggle(language, setLanguage, text.about.pl, text.about.en); 
+                toggle(languageButton, setLanguageButton, 'PL', 'EN');
+                }}>{languageButton}</button>
+            <div className='about-component'>
+                <header className='about-component-header'>TBEBEN</header>
+                <h1 className='about-component-text'>
+                    {language}
+                </h1>
+                <div className="about-component-imgOfMyself" />
+            </div>
+        </>
     )
 }
 
---
+++ src/components/ContentComponents/Language.js
@@ -0,0 +1,18 @@
+export const language={
+    about: {
+        pl: "Nazywam się Tomasz Bęben i polubiłem programowanie. Od ponad roku uczę się React i myślę, że czas pokazać czego się nauczyłem. Witaj na mojej stronie :)",
+        en: "My name is Tomasz Beben and I like programming. I've been learning React for over a year now and I think it's time to show what I ve learned.Welcome to my website :)"
+    },
+    cv:{
+        pl: 'polskie CV',
+        en: 'english CV'
+    },
+    mailingComponent:{
+        pl:'polskie napisy',
+        en: 'english subtitles'
+    },
+    projects:{
+        pl: 'polskie opisy',
+        en: 'english descriptions'
+    }
+}
