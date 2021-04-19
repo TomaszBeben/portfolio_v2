ccf99e12c0c08719c97283c15bad93ed817f29ae
TomaszBeben <tbeben@st.swps.edu.pl>
2021-04-19 21:55:06 +0200

    email-funcionality

--- src/components/ContentComponents/MailingComponent.js
+++ src/components/ContentComponents/MailingComponent.js
@@ -1,11 +1,30 @@
-import React from 'react'
+import React from 'react';
+import emailjs from 'emailjs-com';
 
-const MailingComponent = () => {
-    return (
-        <>
-            tutaj bedzie małe wyzwanie, bo przez ten formularz będą wysyłałay się do mnie emaile
-        </>
-    )
+
+export default function ContactUs() {
+
+  function sendEmail(e) {
+    e.preventDefault();
+
+    emailjs.sendForm('service_3a7gotu', 'template_ovvf9lh', e.target, 'user_qDG873dWKuf3ztHm33w2B')
+      .then((result) => {
+          console.log(result.text);
+      }, (error) => {
+          console.log(error.text);
+      });
+  }
+
+  return (
+    <form className="contact-form" onSubmit={sendEmail}>
+      <input type="hidden" name="contact_number" />
+      <label>Name</label>
+      <input type="text" name="user_name" />
+      <label>Email</label>
+      <input type="text" name="user_email" />
+      <label>Message</label>
+      <textarea wrap='soft' cols='50' rows='5' name="message" />
+      <input type="submit" value="Send" />
+    </form>
+  );
 }
-
-export default MailingComponent
